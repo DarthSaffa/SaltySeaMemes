4a11bbbef06a47b69fffffde5ffc2987069a8ef5
DarthSaffa <rowen123>
2016-11-21 22:31:42 +1000

    Does this shit work?

---
+++ Salty Sea Memes/Assets/Scripts/MyCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCamera : MonoBehaviour {
+
+	Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = transform; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//We want to stop the camera from rotating
+		myTransform.rotation = Quaternion.Euler(0,0,0);
+
+		//Camera needs a constant z position
+		myTransform.position = new Vector3 (myTransform.position.x, myTransform.position.y, -10);
+
+	}
+}
